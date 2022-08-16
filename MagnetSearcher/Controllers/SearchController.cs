ee5faa8c462a3eb91c2d04bd7efac664d397804b
ModerRAS <adqewsx@gmail.com>
2022-08-16 15:41:06 +0800

    有界面了

--- MagnetSearcher/Controllers/SearchController.cs
+++ MagnetSearcher/Controllers/SearchController.cs
@@ -1,9 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyNetQ;
+using MagnetSearcher.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagnetSearcher.Controllers {
     public class SearchController : Controller {
-        public IActionResult Index() {
-            return View();
+        private IBus Bus { get; set; }
+        public SearchController(IBus bus) {
+            this.Bus = bus;
+        }
+        public async Task<IActionResult> Index(string KeyWord, int Skip, int Take) {
+            var option = await TestSearch(KeyWord, Skip, Take);
+            return View(option);
+        }
+        public async Task<ResponseSearchOption> Search(string KeyWord, int Skip, int Take) {
+            var option = await Bus.Rpc.RequestAsync<RequestSearchOption, ResponseSearchOption>(new RequestSearchOption() {
+                KeyWord = KeyWord,
+                Id = Guid.NewGuid(),
+                Skip = Skip,
+                Take = Take
+            });
+            return option;
+        }
+        public async Task<ResponseSearchOption> TestSearch(string KeyWord, int Skip, int Take) {
+            return new ResponseSearchOption() {
+                Count = 2,
+                Id = Guid.NewGuid(),
+                Skip = 0,
+                Take = 20,
+                Info = new List<MagnetInfo>() {
+                    new MagnetInfo() {
+                        InfoHash = "111",
+                        Name = "test",
+                        Length = 200,
+                        GetDateTime = DateTime.Now.Ticks
+                    },
+                    new MagnetInfo() {
+                        InfoHash = "121",
+                        Name = "test2",
+                        Length = 202,
+                        GetDateTime = DateTime.Now.Ticks
+                    },
+                }
+            };
         }
     }
 }
