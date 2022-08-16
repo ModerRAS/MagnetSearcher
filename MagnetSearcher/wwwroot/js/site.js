6e6416f4d35439c425f5d91107703d10d8fb15cb
ModerRAS <adqewsx@gmail.com>
2022-08-16 16:53:26 +0800

    界面装修大半，还有翻页没做

--- MagnetSearcher/wwwroot/js/site.js
+++ MagnetSearcher/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
+function getQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] == variable) {
+            return decodeURIComponent(pair[1].replaceAll("+", " "));
+        }
+    }
+    return (false);
+}
+
+document.getElementsByClassName('search')[0]
+    .addEventListener('click', function (event) {
+        window.location = `//${window.location.host}/Search/?KeyWord=${document.getElementById('searchKeyword').value}&Skip=0&Take=20`
+});
+
+docReady(() => {
+    if (getQueryVariable("KeyWord")) {
+        document.getElementById('searchKeyword').value = getQueryVariable("KeyWord");
+    }
+})
