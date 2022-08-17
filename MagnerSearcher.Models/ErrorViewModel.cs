b1db55073cc22ae6de308941d661f2c0f46f2af6
ModerRAS <adqewsx@gmail.com>
2022-08-17 16:13:47 +0800

    删除无用代码

--- MagnerSearcher.Models/ErrorViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace MagnetSearcher.Models {
-    public class ErrorViewModel {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
