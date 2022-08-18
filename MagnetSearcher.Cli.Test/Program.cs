fe942964300436502040b99aff439fbf3da9a2e0
ModerRAS <adqewsx@gmail.com>
2022-08-18 14:20:09 +0800

    新增了添加测试内容的测试

---
+++ MagnetSearcher.Cli.Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using MagnetSearcher.Models;
+using Newtonsoft.Json;
+
+namespace MagnetSearcher.Cli.Test {
+    internal class Program {
+        static async Task Main(string[] args) {
+            var client = new HttpClient();
+            
+            for (int i = 0; i < 1000; i++) {
+                var result = await client.PostAsync(
+                "http://127.0.0.1:5257/v1/DHT/add/12345",
+                new StringContent(
+                    JsonConvert.SerializeObject(new MagnetInfo() {
+                        InfoHash = i.ToString(),
+                        Name = i.ToString(),
+                        Length = 200,
+                        GetDateTime = DateTime.Now.Ticks,
+                        Files = new List<MagnetFile>() {
+                            new MagnetFile() {
+                                Path = new List<string>() {
+                                    "1.txt"
+                                },
+                                Length = 200
+                            }
+                        },
+                        RawMetaDataBase64 = "123"
+                    }), 
+                    System.Text.Encoding.UTF8, 
+                    "application/json"
+                    )
+                );
+                Console.WriteLine(await result.Content.ReadAsStringAsync());
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
