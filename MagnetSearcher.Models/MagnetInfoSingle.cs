f02d5aaac93f43fbac304289fd7e5e5d1fbec303
ModerRAS <adqewsx@gmail.com>
2022-08-20 15:49:56 +0800

    添加单文件输入方式

---
+++ MagnetSearcher.Models/MagnetInfoSingle.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MagnetSearcher.Models {
+    public class MagnetInfoSingle {
+        [JsonProperty(PropertyName = "infohash")]
+        public string InfoHash { get; set; }
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+        [JsonProperty(PropertyName = "length")]
+        public long Length { get; set; }
+        [JsonProperty(PropertyName = "rawmetadatabase64")]
+        public string RawMetaDataBase64 { get; set; }
+        [JsonProperty(PropertyName = "getdatetime")]
+        public long GetDateTime { get; set; }
+        public DateTime DateTime { 
+            get { 
+                return TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)).Add(new TimeSpan(GetDateTime * 10000000)); 
+            } 
+        }
+    }
+}
--- MagnetSearcher/Controllers/DHTController.cs
+++ MagnetSearcher/Controllers/DHTController.cs
@@ -21,5 +21,22 @@ namespace MagnetSearcher.Controllers {
                 return Forbid();
             }
         }
+        [HttpPost("add/{token}")]
+        public async Task<IActionResult> AddMagnetSingle(string token, [FromBody] MagnetInfoSingle infosingle) {
+            if (token.Equals(Env.RootToken) || Env.RootToken.Equals(string.Empty)) {
+                var info = new MagnetInfo() {
+                    InfoHash = infosingle.InfoHash,
+                    Files = new List<MagnetFile>(),
+                    GetDateTime = infosingle.GetDateTime,
+                    Length = infosingle.Length,
+                    Name = infosingle.Name,
+                    RawMetaDataBase64 = infosingle.RawMetaDataBase64
+                };
+                await Bus.PubSub.PublishAsync(info);
+                return Ok(infosingle);
+            } else {
+                return Forbid();
+            }
+        }
     }
 }
