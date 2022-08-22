ee446d7bb8f0b1d9f8352f7a955611302f764346
ModerRAS <adqewsx@gmail.com>
2022-08-22 11:10:36 +0800

    添加接收EasyNetQ的默认错误的功能

---
+++ MagnetSearcher.Daemon/Controllers/ErrorController.cs
@@ -0,0 +1,35 @@
+﻿using EasyNetQ;
+using EasyNetQ.SystemMessages;
+using Queue = EasyNetQ.Topology.Queue;
+using MagnetSearcher.Daemon.Interfaces;
+using MagnetSearcher.Daemon.Services;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagnetSearcher.Daemon.Controllers {
+    public class ErrorController : IController {
+        private IBus Bus { get; set; }
+        private ILogger Logger { get; set; }
+        private ErrorService ErrorService { get; set; }
+        private const string QueueName = "EasyNetQ_Default_Error_Queue";
+        public ErrorController(IBus bus, ErrorService errorService, ILogger logger) {
+            Bus = bus;
+            ErrorService = errorService;
+            Logger = logger;
+        }
+        public async Task ExecAsync() {
+
+            var queue = new Queue(QueueName, false);
+
+            Action<IMessage<Error>, MessageReceivedInfo> foo = (error, info) => {
+                Logger.Log(LogLevel.Information, error.Body.Message);
+            };
+
+            Bus.Advanced.Consume<Error>(queue, foo);
+        }
+    }
+}
